cfc6790e4abe03562fe3e581762b9dde46a84db4
MuhteremKuzucu <kuzucumuhterem@gmail.com>
2025-03-10 14:48:50 +0100

    up

--- 10-Error_Management/error.js
+++ 10-Error_Management/error.js
@@ -20,11 +20,14 @@ const PORT = process.env.PORT || 8000;
 
 //? throw
 app.get('/user/:id', (req, res) => {
+
+   
     
     const id = req.params.id
 
     if(isNaN(id)) {
-        throw new Error('ID is not a number')
+        res.errorStatusCode = 400
+        throw new Error('ID is not a number', {cause: 'params.id=' + id})
     } else {
         res.send({
             error: false,
@@ -32,7 +35,7 @@ app.get('/user/:id', (req, res) => {
         });
     }
 });
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 //? try-catch
 
 app.get('/user/:id', (req, res) => {
@@ -49,20 +52,62 @@ app.get('/user/:id', (req, res) => {
             });
         }
         
-    } catch (error) {
-        console.log(error);
-        res.send({
-            error: true,
-            message: error.message
-        })
+    } catch (err) {
+        // console.log(error);
+        // res.send({
+        //     error: true,
+        //     message: error.message
+        // })
+        //* to send this error to error handler middleware
+        next(err)
         
     }
 
     
 });
+/* ------------------------------------------------------- *
+//? async-functions
+
+const asyncFn = async () =>{
+
+    throw new Error('Created error in async-fn')
+}
+
+app.get('/async', async(req, res, next) =>{
+    await asyncFn()
+        .then()
+        .catch(next)
+})
+
+/* ------------------------------------------------------- */
+//? express-async-errors
+require('express-async-errors');
+
+app.get('/async', async(req, res, next) =>{
+    res.errorStatusCode = 400
+    throw new Error ('Created error in self-async-fn')
+})
 
 
+/* ------------------------------------------------------- */
 
+//? Error Handler Middleware
+//* Error handler kodların en altında bulunmalı
+
+const errorHandler = (err, req, res, next) =>{
+
+    console.log('error handler worked');
+    const statusCode = res.errorStatusCode ?? 500
+
+    res.status(statusCode).send({
+        error: true,
+        message: err.message,
+        cause: err.cause, // This discribes the reason of the error
+        //stack: err.stack // Error Details
+    })
+};
+
+app.use(errorHandler);
 
 
 /* ------------------------------------------------------- */
